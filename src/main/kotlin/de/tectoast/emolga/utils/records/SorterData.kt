31856146d64343524b57b0ce749d5858f2e891c0
Flo <tectoast@gmail.com>
2022-07-27 18:04:57 +0200

    fix: forgot code to commit 70994a61a681bec0cdad329a9065dae748a9918f

--- src/main/kotlin/de/tectoast/emolga/utils/records/SorterData.kt
+++ src/main/kotlin/de/tectoast/emolga/utils/records/SorterData.kt
@@ -4,19 +4,17 @@ import java.util.function.Function
 
 class SorterData(
     val formulaRange: List<String>,
-    val pointRange: List<String>,
     val directCompare: Boolean,
     val indexer: Function<String, Int>?,
     vararg val cols: Int
 ) {
     constructor(
         formulaRange: String,
-        pointRange: String,
         directCompare: Boolean,
         indexer: Function<String, Int>?,
         vararg cols: Int
     ) : this(
-        listOf<String>(formulaRange), listOf<String>(pointRange),
+        listOf<String>(formulaRange),
         directCompare, indexer, *cols
     )
 }

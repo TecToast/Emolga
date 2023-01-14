18ac8ce067fafd5e746a1193f7c2ff93045697a4
Flo <tectoast@gmail.com>
2023-01-14 13:09:46 +0100

    build: bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val kVersion = "1.7.10"
+val kVersion = "1.8.0"
 plugins {
-    val kVersion = "1.7.10"
+    val kVersion = "1.8.0"
     kotlin("jvm") version kVersion
     kotlin("plugin.serialization") version kVersion
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -54,7 +54,7 @@ repositories {
     maven("https://jitpack.io/")
 }
 
-val ktorVersion = "2.1.3"
+val ktorVersion = "2.2.2"
 val ktorDependencies = listOf(
     // Client
     "ktor-client-core",
@@ -72,13 +72,11 @@ val ktorDependencies = listOf(
 )
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:2.0.5")
-    implementation("org.eclipse.jetty:jetty-server:11.0.12")
-    implementation("org.eclipse.jetty:jetty-util:11.0.12")
+    implementation("org.slf4j:slf4j-api:2.0.6")
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
     implementation("com.google.apis:google-api-services-docs:v1-rev20221004-2.0.0")
-    implementation("com.google.apis:google-api-services-sheets:v4-rev20220927-2.0.0")
-    implementation("com.google.apis:google-api-services-youtube:v3-rev20220926-2.0.0")
+    implementation("com.google.apis:google-api-services-sheets:v4-rev20221216-2.0.0")
+    implementation("com.google.apis:google-api-services-youtube:v3-rev20221108-2.0.0")
     implementation("org.jsoup:jsoup:1.15.3")
     implementation("com.sedmelluq:lavaplayer:1.3.78")
     //implementation("org.slf4j:slf4j-simple:1.7.32")
@@ -91,18 +89,12 @@ dependencies {
     //implementation("com.github.TecToast:JSOLF:a43c3e06c7")
     implementation("net.dv8tion:JDA:5.0.0-beta.2")
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.0.8")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.1")
     implementation("com.github.MinnDevelopment:jda-ktx:0.9.6-alpha.22")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kVersion")
-    implementation("io.ktor:ktor-server-auth-jvm:2.1.3")
-    implementation("io.ktor:ktor-server-locations-jvm:2.1.3")
-    implementation("io.ktor:ktor-server-core-jvm:2.1.3")
-    implementation("io.ktor:ktor-client-core-jvm:2.1.3")
-    implementation("io.ktor:ktor-client-apache-jvm:2.1.3")
-    implementation("io.ktor:ktor-server-cors-jvm:2.1.3")
     ktor()
     implementation("org.jetbrains.exposed:exposed-core:0.40.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.40.1")

ab7b97b74a971a204e8710c92bcbd402c85698a4
Flo <tectoast@gmail.com>
2023-04-10 09:07:03 +0200

    fix: SDNames id check

--- src/main/kotlin/de/tectoast/emolga/database/exposed/SDNamesDB.kt
+++ src/main/kotlin/de/tectoast/emolga/database/exposed/SDNamesDB.kt
@@ -1,5 +1,6 @@
 package de.tectoast.emolga.database.exposed
 
+import de.tectoast.emolga.commands.Command
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
@@ -9,7 +10,8 @@ object SDNamesDB : Table("sdnames") {
     val NAME = varchar("name", 18)
     val ID = long("id")
 
-    fun getIDByName(name: String) = transaction { select { NAME eq name }.firstOrNull()?.get(ID) } ?: -1
+    fun getIDByName(name: String) =
+        transaction { select { NAME eq Command.toUsername(name) }.firstOrNull()?.get(ID) } ?: -1
 
     fun addIfAbsent(name: String, id: Long) = transaction {
         (select { NAME eq name }.firstOrNull() == null).also { b ->

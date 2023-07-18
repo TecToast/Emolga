f2ef35fe1cc022cd9b35f117dc08d64f2e431645
Flo <tectoast@gmail.com>
2023-07-18 12:28:00 +0200

    feat: implement feature to allow FinishDraft even it isn't your turn

--- src/main/kotlin/de/tectoast/emolga/commands/draft/FinishDraftCommand.kt
+++ src/main/kotlin/de/tectoast/emolga/commands/draft/FinishDraftCommand.kt
@@ -3,31 +3,28 @@ package de.tectoast.emolga.commands.draft
 import de.tectoast.emolga.commands.Command
 import de.tectoast.emolga.commands.CommandCategory
 import de.tectoast.emolga.commands.GuildCommandEvent
-import de.tectoast.emolga.commands.saveEmolgaJSON
 import de.tectoast.emolga.utils.json.emolga.draft.League
+import dev.minn.jda.ktx.coroutines.await
 
 class FinishDraftCommand :
     Command("finishdraft", "Beendet für dich den Draft", CommandCategory.Draft) {
     init {
-        aliases.add("finish")
         argumentTemplate = ArgumentManagerTemplate.noArgs()
         slash(true, *draftGuilds)
     }
 
     override suspend fun process(e: GuildCommandEvent) {
-        val d =
-            League.byCommand(e) ?: return e.reply("Es läuft zurzeit kein Draft in diesem Channel!", ephemeral = true)
-        if (d.isFinishedForbidden()) {
-            e.reply("Dieser Draft unterstützt /finish nicht!")
-            return
-        }
-        val mem = d.current
+        val mem = e.author.idLong
+        val d = League.onlyChannel(e.textChannel.idLong)?.takeIf { mem in it.table } ?: return e.reply(
+            "In diesem Channel läuft kein Draft, an welchem du teilnimmst!",
+            ephemeral = true
+        )
+        if (d.isFinishedForbidden()) return e.reply("Dieser Draft unterstützt /finishdraft nicht!")
         d.checkFinishedForbidden(mem)?.let {
             return e.reply(it)
         }
-        d.replyFinish(e)
+        e.slashCommandEvent!!.reply("${e.member.asMention} hat den Draft für sich beendet!").await()
         d.addFinished(mem)
         d.nextPlayer()
-        saveEmolgaJSON()
     }
 }

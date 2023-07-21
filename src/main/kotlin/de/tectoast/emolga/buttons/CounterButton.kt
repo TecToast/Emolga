ab72d712c2600a4d1588668ed3511060a3eafa0f
Flo <tectoast@gmail.com>
2023-07-21 12:24:07 +0200

    refactor: shinycount uses mongodb refactor: remove movefilter

--- src/main/kotlin/de/tectoast/emolga/buttons/CounterButton.kt
+++ src/main/kotlin/de/tectoast/emolga/buttons/CounterButton.kt
@@ -3,14 +3,18 @@ package de.tectoast.emolga.buttons
 import de.tectoast.emolga.commands.Command
 import de.tectoast.emolga.commands.ifMatches
 import de.tectoast.emolga.utils.json.Shinycount
+import de.tectoast.emolga.utils.json.db
+import de.tectoast.emolga.utils.json.updateOnly
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
+import org.litote.kmongo.inc
+import org.litote.kmongo.keyProjection
 
 class CounterButton : ButtonListener("counter") {
     override suspend fun process(e: ButtonInteractionEvent, name: String) {
         val split = name.split(":")
         val method = split[0]
         val mem = e.user.idLong.ifMatches(598199247124299776) { it == 893773494578470922 }.toString()
-        Shinycount.get.counter[method]!!.compute(mem) { _, v -> (v ?: 0) + split[1].toInt() }
+        db.shinycount.updateOnly(inc(Shinycount::counter.keyProjection(method).keyProjection(mem), split[1].toInt()))
         Command.updateShinyCounts(e)
     }
 }

a7152348bf701f5abaf2b6f1a0b05582335e952d
Flo <tectoast@gmail.com>
2023-08-19 13:51:01 +0200

    fix: PokeFansExportCommand

--- src/main/kotlin/de/tectoast/emolga/commands/draft/PokeFansExportCommand.kt
+++ src/main/kotlin/de/tectoast/emolga/commands/draft/PokeFansExportCommand.kt
@@ -4,6 +4,8 @@ import de.tectoast.emolga.bot.EmolgaMain
 import de.tectoast.emolga.commands.*
 import de.tectoast.emolga.utils.json.db
 import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.add
 import kotlinx.serialization.json.buildJsonArray
 import java.util.regex.Pattern
@@ -17,41 +19,50 @@ class PokeFansExportCommand : Command("pokefansexport", "Macht Pokefans Export l
             ArgumentManagerTemplate.draft()
         ).setExample("!pokefansexport Emolga-Conference").build()
         setCustomPermissions(PermissionPreset.CULT)
+        slash()
     }
 
     override suspend fun process(e: GuildCommandEvent) {
+        e.done(ephemeral = true)
+        val tc = e.textChannel
         val league = db.league(e.arguments.getText("draft"))
         val picksObj = league.picks
-        val tosend = mutableListOf<Any>()
+        val tosend = mutableListOf<JsonElement>()
         val ids = ArrayList(picksObj.keys)
         val names = mutableMapOf<Long, String>()
         EmolgaMain.emolgajda.getGuildById(league.guild)!!.retrieveMembersByIds(ids).get()
             .forEach { names[it.idLong] = it.effectiveName }
         for (id in ids) {
             val picksArr = picksObj[id].names().map { it to getDataObject(it).speed }
-            val oneUser = mutableListOf<Any>()
-            oneUser.add(JUST_CHARS_AND_WHITESPACES.matcher(names[id]!!).replaceAll(""))
-            oneUser.add(e.getArg(0))
-            val mons = buildJsonArray {
-                picksArr.asSequence().sortedByDescending {
-                    it.second
-                }.map {
-                    it.first.replace("-T", " Tiergeistform")
-                        .replace("-I", " Inkarnationsform")
-                        .replace("Wolwerock-Tag", "Wolwerock Tagform").replace("Wolwerock-Nacht", "Wolwerock Nachtform")
-                        .replace("Wolwerock-Zw", "Wolwerock Zwielichtform").replace("Shaymin", "Shaymin Landform")
-                        .replace("Durengard", "Durengard Schildform").replace("Pumpdjinn", "Pumpdjinn XL")
-                        .replace("M-", "Mega-").replace("A-", "Alola-").replace("G-", "Galar-")
-                }.forEach { add(it) }
+            val oneUser = buildJsonArray {
+                add(JUST_CHARS_AND_WHITESPACES.matcher(names[id]!!).replaceAll(""))
+                add(e.getArg(0))
+                val mons = buildJsonArray {
+                    picksArr.asSequence().sortedByDescending {
+                        it.second
+                    }.map {
+                        it.first.replace("-Therian", " Tiergeistform")
+                            .replace("Wolwerock-Midnight", "Wolwerock Nachtform")
+                            .replace("Wolwerock-Dusk", "Wolwerock Zwielichtform").replace("Shaymin", "Shaymin Landform")
+                            .replace("Durengard", "Durengard Schildform").replace("Pumpdjinn", "Pumpdjinn XL")
+                            .let { str ->
+                                var tmp = str
+                                for (suffix in listOf("Mega", "Alola", "Galar", "Hisui", "Paldea")) {
+                                    if (tmp.endsWith("-$suffix")) tmp = suffix + "-" + tmp.substringBeforeLast("-")
+                                }
+                                tmp
+                            }
+                    }.forEach { add(it) }
+                }
+                add(mons)
             }
-            oneUser.add(mons)
             tosend.add(oneUser)
-            if (tosend.toString().length > 1500) {
-                e.reply(myJSON.encodeToString(tosend))
-                tosend.clear()
-            }
         }
-        e.reply(tosend.toString())
+        tc.sendMessage("[").queue()
+        tosend.chunked(2).forEach {
+            tc.sendMessage(myJSON.encodeToString(JsonArray(it)).removeSurrounding("[", "]") + ",").queue()
+        }
+        tc.sendMessage("]").queue()
     }
 
     companion object {

c782e83b694edfaab39211035b7fd0b036509a0e
florian <tectoast@gmail.com>
2023-09-27 09:18:40 +0200

    fix: conference choosing didn't save

--- src/main/kotlin/de/tectoast/emolga/selectmenus/CSelectMenu.kt
+++ src/main/kotlin/de/tectoast/emolga/selectmenus/CSelectMenu.kt
@@ -9,9 +9,11 @@ import dev.minn.jda.ktx.messages.Embed
 import dev.minn.jda.ktx.messages.editMessage
 import dev.minn.jda.ktx.messages.into
 import dev.minn.jda.ktx.messages.send
+import mu.KotlinLogging
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent
 
 class CSelectMenu : MenuListener("cselect") {
+    private val logger = KotlinLogging.logger {}
     override suspend fun process(e: StringSelectInteractionEvent, menuname: String?) {
         val (mode, uid) = menuname!!.split(":").let { it[0] to it[1].toLong() }
         val gid = e.guild?.idLong ?: return
@@ -20,6 +22,7 @@ class CSelectMenu : MenuListener("cselect") {
         val oldConf = user.conference.indexedBy(data.conferences)
         user.conference = e.values.first()
         val newConf = user.conference.indexedBy(data.conferences)
+        data.save()
         if (mode == "initial") {
             val nextuser = data.users.entries.firstOrNull { it.value.conference == null }?.key ?: run {
                 val tc = e.jda.getTextChannelById(data.shiftChannel!!)!!
@@ -34,6 +37,7 @@ class CSelectMenu : MenuListener("cselect") {
             e.deferEdit().queue()
             e.hook.deleteOriginal().queue()
             PrivateCommands.generateOrderingMessages(data, oldConf, newConf).forEach {
+                logger.info("Editing message ${it.key} with ${it.value.first.title}")
                 e.channel.editMessage(
                     data.shiftMessageIds[it.key].toString(),
                     embeds = it.value.first.into(),

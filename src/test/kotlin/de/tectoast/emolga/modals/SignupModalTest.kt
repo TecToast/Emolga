63c09d9dbb290568795f8fce30851ef7a241b9e4
Flo <tectoast@gmail.com>
2023-10-15 12:03:00 +0200

    fix: don't init Mongo in test as it is initialized before anything

--- src/test/kotlin/de/tectoast/emolga/modals/SignupModalTest.kt
+++ src/test/kotlin/de/tectoast/emolga/modals/SignupModalTest.kt
@@ -1,12 +1,8 @@
 package de.tectoast.emolga.modals
 
-import de.tectoast.emolga.utils.json.initMongo
 import io.kotest.core.spec.style.FunSpec
 
 class SignupModalTest : FunSpec({
-    beforeSpec {
-        initMongo("mongodb://localhost:27017/", "emolgatest")
-    }
     test("SignupFast") {
 
     }

46bae378d029d81d6ba7f1a36c511391084c5a44
Flo <tectoast@gmail.com>
2023-11-07 22:44:52 +0100

    build: bump to Kotlin 1.9.20 & Gradle 8.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val kVersion = "1.9.0"
+val kVersion = "1.9.20"
 plugins {
-    val kVersion = "1.9.0"
+    val kVersion = "1.9.20"
     kotlin("jvm") version kVersion
     kotlin("plugin.serialization") version kVersion
     id("com.github.johnrengelman.shadow") version "8.1.1"

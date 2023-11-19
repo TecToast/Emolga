e997727ff0317d6d18078beb2e8ddaffca1b0cb3
Flo <tectoast@gmail.com>
2023-11-19 12:22:43 +0100

    feat: implement test for message attachments on slash commands

--- src/test/kotlin/de/tectoast/emolga/JDATesting.kt
+++ src/test/kotlin/de/tectoast/emolga/JDATesting.kt
@@ -1,5 +1,7 @@
 package de.tectoast.emolga
 
 import de.tectoast.emolga.bot.jda
+import de.tectoast.emolga.utils.Constants
 
+val defaultGuild by lazy { jda.getGuildById(Constants.G.MY)!! }
 val defaultChannel by lazy { jda.getTextChannelById(1171544197573116004)!! }

c392056add59a01a889d3e7151b79d97a333c9f3
Flo <tectoast@gmail.com>
2023-12-02 14:20:05 +0100

    fix: MatchaGotcha Burn was not associated with Sinistcha

--- src/main/kotlin/de/tectoast/emolga/utils/showdown/AnalysisEngine.kt
+++ src/main/kotlin/de/tectoast/emolga/utils/showdown/AnalysisEngine.kt
@@ -112,9 +112,8 @@ sealed class SDEffect(vararg val types: String) {
             it.itemObtainedFrom.takeIf { "item:" in inlined[0] } ?: it
         }
         this.firstOrNull { it.startsWith("[of] p") }?.let { return it.parsePokemon(ctx) }
-        return ctx.takeIf { it.lastLine.startsWith("|-damage") || it.lastLine.startsWith("|move") }?.lastMove?.cleanSplit()
-            ?.getOrNull(1)?.parsePokemon(ctx) ?: ctx.lastLine.cleanSplit().takeIf { it.getOrNull(0) == "-activate" }
-            ?.getOrNull(1)?.parsePokemon(ctx)
+        return ctx.lastLine.cleanSplit().takeIf { it.getOrNull(0) == "-activate" }
+            ?.getOrNull(1)?.parsePokemon(ctx) ?: ctx.lastMove.cleanSplit().getOrNull(1)?.parsePokemon(ctx)
     }
 
     data object Turn : SDEffect("turn") {

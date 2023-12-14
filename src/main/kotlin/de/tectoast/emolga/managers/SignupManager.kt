63a742fc9e036780c78565904793ae9195397755
florian <tectoast@gmail.com>
2023-12-14 22:31:26 +0100

    refactor: move all logic into SignupManager

--- src/main/kotlin/de/tectoast/emolga/managers/SignupManager.kt
+++ src/main/kotlin/de/tectoast/emolga/managers/SignupManager.kt
@@ -1,11 +1,28 @@
 package de.tectoast.emolga.managers
 
 import de.tectoast.emolga.bot.jda
+import de.tectoast.emolga.commands.Command
+import de.tectoast.emolga.commands.PrivateCommands
+import de.tectoast.emolga.database.exposed.SDNamesDB
+import de.tectoast.emolga.utils.Constants
 import de.tectoast.emolga.utils.json.LigaStartData
+import de.tectoast.emolga.utils.json.SignUpData
 import de.tectoast.emolga.utils.json.db
+import de.tectoast.emolga.utils.json.get
 import dev.minn.jda.ktx.coroutines.await
 import dev.minn.jda.ktx.interactions.components.primary
+import dev.minn.jda.ktx.messages.reply_
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import net.dv8tion.jda.api.entities.UserSnowflake
 import net.dv8tion.jda.api.entities.emoji.Emoji
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent
+import java.util.concurrent.ConcurrentHashMap
 
 object SignupManager {
     suspend fun createSignup(
@@ -39,5 +56,82 @@ object SignupManager {
                 withExperiences = withExperiences
             )
         )
+    }
+
+    private val persistentSignupData = ConcurrentHashMap<Long, Pair<Mutex, Channel<LigaStartData>>>()
+    private val signupScope = CoroutineScope(Dispatchers.IO)
+
+    suspend fun signupUser(
+        gid: Long,
+        uid: Long,
+        sdname: String,
+        teamname: String?,
+        experiences: String?,
+        isChange: Boolean = false,
+        e: ModalInteractionEvent? = null
+    ): Unit? {
+        val sdnameid = Command.toUsername(sdname)
+        if (sdnameid.length !in 1..18) return e?.reply_("Dieser Showdown-Name ist ungültig!")?.setEphemeral(true)
+            ?.queue()
+        e?.deferReply(true)?.queue()
+        val (signupMutex, channel) = persistentSignupData.getOrPut(gid) {
+            val c = Channel<LigaStartData>(Channel.CONFLATED)
+            signupScope.launch {
+                while (true) {
+                    c.receive().updateSignupMessage()
+                    delay(10000)
+                }
+            }
+            Mutex() to c
+        }
+        signupMutex.withLock {
+            with(db.signups.get(gid)!!) {
+                if (!isChange && full) return e?.hook?.sendMessage("❌ Die Anmeldung ist bereits voll!")?.queue()
+                val ownerOfTeam = PrivateCommands.userIdForSignupChange?.takeIf { uid == Constants.FLOID }
+                    ?: if (isChange) getOwnerByUser(uid) ?: return e?.reply_(
+                        "Du bist derzeit nicht angemeldet!"
+                    )?.setEphemeral(true)?.queue()
+                    else uid
+                if (ownerOfTeam in users && !isChange) return e?.reply_("Du bist bereits angemeldet!")
+                    ?.setEphemeral(true)?.queue()
+
+                @Suppress("DeferredResultUnused") SDNamesDB.addIfAbsent(sdname, ownerOfTeam)
+                val jda = e?.jda ?: jda
+                if (isChange) {
+                    val data = users[ownerOfTeam]!!
+                    data.sdname = sdname
+                    data.teamname = teamname
+                    data.experiences = experiences
+                    e?.hook?.sendMessage("Deine Daten wurden erfolgreich geändert!")?.queue()
+                    jda.getTextChannelById(signupChannel)!!.editMessageById(
+                        data.signupmid!!, data.toMessage(ownerOfTeam, this)
+                    ).queue()
+                    data.logomid?.let {
+                        jda.getTextChannelById(logoChannel)!!
+                            .editMessageById(it, "**Logo von <@$ownerOfTeam> ($teamname):**").queue()
+                    }
+                    save()
+                    return null
+                }
+                e?.hook?.sendMessage("✅ Du wurdest erfolgreich angemeldet!")?.setEphemeral(true)?.queue()
+                giveParticipantRole(
+                    (e?.member) ?: jda.getGuildById(gid)?.retrieveMember(UserSnowflake.fromId(uid))!!.await()
+                )
+                val signUpData = SignUpData(
+                    teamname, sdname, experiences = experiences
+                ).apply {
+                    signupmid = jda.getTextChannelById(signupChannel)!!.sendMessage(
+                        toMessage(ownerOfTeam, this@with)
+                    ).await().idLong
+                }
+                users[ownerOfTeam] = signUpData
+                channel.send(this)
+                if (full) {
+                    closeSignup()
+                }
+                save()
+            }
+        }
+        return null
     }
 }
--- src/main/kotlin/de/tectoast/emolga/modals/SignupModal.kt
+++ src/main/kotlin/de/tectoast/emolga/modals/SignupModal.kt
@@ -1,97 +1,29 @@
 package de.tectoast.emolga.modals
 
-import de.tectoast.emolga.commands.Command
-import de.tectoast.emolga.commands.PrivateCommands
 import de.tectoast.emolga.commands.condAppend
-import de.tectoast.emolga.database.exposed.SDNamesDB
-import de.tectoast.emolga.utils.Constants
+import de.tectoast.emolga.managers.SignupManager
 import de.tectoast.emolga.utils.json.LigaStartData
 import de.tectoast.emolga.utils.json.SignUpData
-import de.tectoast.emolga.utils.json.db
-import de.tectoast.emolga.utils.json.get
-import dev.minn.jda.ktx.coroutines.await
 import dev.minn.jda.ktx.interactions.components.Modal
-import dev.minn.jda.ktx.messages.reply_
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent
-import java.util.concurrent.ConcurrentHashMap
 
 object SignupModal : ModalListener("signup") {
-    private val persistentSignupData = ConcurrentHashMap<Long, Pair<Mutex, Channel<LigaStartData>>>()
+
     override suspend fun process(e: ModalInteractionEvent, name: String?) {
         val teamname = e.getValue("teamname")?.asString
         val sdname = e.getValue("sdname")!!.asString
         val experiences = e.getValue("experiences")?.asString
-        val sdnameid = Command.toUsername(sdname)
-        if (sdnameid.length !in 1..18) return e.reply_("Dieser Showdown-Name ist ungültig!").setEphemeral(true).queue()
-        val uid = e.user.idLong
-        val isChange = name == "change"
-        val g = e.guild!!
-        e.deferReply(true).queue()
-        val (signupMutex, channel) = persistentSignupData.getOrPut(g.idLong) {
-            val c = Channel<LigaStartData>(CONFLATED)
-            signupScope.launch {
-                while (true) {
-                    c.receive().updateSignupMessage()
-                    delay(10000)
-                }
-            }
-            Mutex() to c
-        }
-        signupMutex.withLock {
-            with(db.signups.get(g.idLong)!!) {
-                if (!isChange && full) return e.hook.sendMessage("❌ Die Anmeldung ist bereits voll!").queue()
-                val ownerOfTeam = PrivateCommands.userIdForSignupChange?.takeIf { uid == Constants.FLOID }
-                    ?: if (isChange) getOwnerByUser(uid) ?: return e.reply_(
-                        "Du bist derzeit nicht angemeldet!"
-                    ).setEphemeral(true).queue()
-                    else uid
-
-                @Suppress("DeferredResultUnused") SDNamesDB.addIfAbsent(sdname, ownerOfTeam)
-                if (isChange) {
-                    val data = users[ownerOfTeam]!!
-                    data.sdname = sdname
-                    data.teamname = teamname
-                    data.experiences = experiences
-                    e.hook.sendMessage("Deine Daten wurden erfolgreich geändert!").queue()
-                    e.jda.getTextChannelById(signupChannel)!!.editMessageById(
-                        data.signupmid!!, data.toMessage(ownerOfTeam, this)
-                    ).queue()
-                    data.logomid?.let {
-                        e.jda.getTextChannelById(logoChannel)!!
-                            .editMessageById(it, "**Logo von <@$ownerOfTeam> ($teamname):**").queue()
-                    }
-                    save()
-                    return
-                }
-                e.hook.sendMessage("✅ Du wurdest erfolgreich angemeldet!").setEphemeral(true).queue()
-                giveParticipantRole(e.member!!)
-                val signUpData = SignUpData(
-                    teamname, sdname, experiences = experiences
-                ).apply {
-                    signupmid = e.jda.getTextChannelById(signupChannel)!!.sendMessage(
-                        toMessage(ownerOfTeam, this@with)
-                    ).await().idLong
-                }
-                users[ownerOfTeam] = signUpData
-                channel.send(this)
-                if (full) {
-                    closeSignup()
-                }
-                save()
-            }
-        }
+        SignupManager.signupUser(
+            e.guild!!.idLong,
+            e.user.idLong,
+            sdname,
+            teamname,
+            experiences,
+            isChange = name == "change",
+            e = e
+        )
     }
 
-
-    private val signupScope = CoroutineScope(Dispatchers.IO)
     fun getModal(data: SignUpData?, lsData: LigaStartData) =
         Modal("signup".condAppend(data != null, ";change"), "Anmeldung".condAppend(data != null, "sanpassung")) {
             if (!lsData.noTeam) short("teamname", "Team-Name", required = true, value = data?.teamname)

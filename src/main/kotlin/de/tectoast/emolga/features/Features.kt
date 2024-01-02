b3b096b72ca3f17933c3b743873921fa292654f5
florian <tectoast@gmail.com>
2024-01-02 15:20:45 +0100

    feat: will be squashed

--- src/main/kotlin/de/tectoast/emolga/features/Features.kt
+++ src/main/kotlin/de/tectoast/emolga/features/Features.kt
@@ -16,20 +16,32 @@ interface FeatureSpec
 data class CommandSpec(val name: String, val help: String) : FeatureSpec
 
 open class Arguments {
+    val args = mutableListOf<AbstractArg<*>>()
+    fun string(name: String, help: String, builder: AbstractArg<String>.() -> Unit = {}) =
+        createArg(name, help, builder)
 
+    private fun <T> createArg(name: String, help: String, builder: AbstractArg<T>.() -> Unit) =
+        (object : AbstractArg<T>() {
+
+        }).also {
+            it.builder()
+            args += it
+        }
 }
 
 class TestCommandArgs : Arguments() {
-    val test by StringArg()
-}
-
-abstract class AbstractArg<T> : ReadOnlyProperty<Arguments, T> {
-    override fun getValue(thisRef: Arguments, property: KProperty<*>): T {
-
+    val test by string("yay", "nay")
+    val anotherOne by string("huhu", "lol") {
+        defaultValue = test
     }
 }
 
-class StringArg : AbstractArg<String>() {
+abstract class AbstractArg<T> : ReadOnlyProperty<Arguments, T> {
+    private var parsed: T? = null
+    private var success = false
+    var defaultValue: T? = null
 
-
+    override fun getValue(thisRef: Arguments, property: KProperty<*>): T {
+        return if (success) parsed!! else defaultValue!!
+    }
 }

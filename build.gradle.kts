4011c26ae7c1e4acbec69e0cbcc6806cc556297f
Flo <tectoast@gmail.com>
2024-02-12 18:36:23 +0100

    build: add coroutines test lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -110,6 +110,7 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.6.2")
     testImplementation("io.kotest:kotest-assertions-core:5.6.2")
     testImplementation("io.kotest:kotest-framework-datatest:5.6.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0-RC2")
 
 }
 

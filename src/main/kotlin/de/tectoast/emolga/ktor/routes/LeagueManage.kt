8a5cbe4d8da08cbcb7438e67860f4b2b8dab0694
Flo <tectoast@gmail.com>
2024-02-24 01:01:55 +0100

    feat: add logoUrl to LeagueData and fill it with a placeholder

--- src/main/kotlin/de/tectoast/emolga/ktor/routes/LeagueManage.kt
+++ src/main/kotlin/de/tectoast/emolga/ktor/routes/LeagueManage.kt
@@ -14,11 +14,16 @@ fun Route.leagueManage() {
     get("/myleagues") {
         val u = userId()
         val send = db.drafts.find(League::table contains u).toList().map {
-            LeagueData(it.leaguename, it.providePicksForGameday(1)[u]!!, it.isRunning)
+            LeagueData(
+                it.leaguename,
+                it.providePicksForGameday(1)[u]!!,
+                it.isRunning,
+                "https://cdn.discordapp.com/icons/518008523653775366/2f9ba8f68d7b468f5945c54fcb61c46a.png"
+            )
         }.sortedByDescending { it.isRunning }
         call.respond(send)
     }
 }
 
 @Serializable
-data class LeagueData(val name: String, val picks: List<DraftPokemon>, val isRunning: Boolean)
+data class LeagueData(val name: String, val picks: List<DraftPokemon>, val isRunning: Boolean, val logoUrl: String)

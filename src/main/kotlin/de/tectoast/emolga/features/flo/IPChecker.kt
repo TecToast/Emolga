04657bd0c66011ea56fc321ed924146b46129d3a
Flo <tectoast@gmail.com>
2024-04-02 01:38:45 +0200

    feat: IPChecker

---
+++ src/main/kotlin/de/tectoast/emolga/features/flo/IPChecker.kt
@@ -0,0 +1,22 @@
+package de.tectoast.emolga.features.flo
+
+import de.tectoast.emolga.features.ButtonFeature
+import de.tectoast.emolga.features.ButtonSpec
+import de.tectoast.emolga.features.InteractionData
+import de.tectoast.emolga.features.NoArgs
+import de.tectoast.emolga.utils.httpClient
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
+object IPChecker : ButtonFeature<NoArgs>(NoArgs(), ButtonSpec("ipchecker")) {
+
+    init {
+        restrict(flo)
+    }
+
+    context(InteractionData)
+    override suspend fun exec(e: NoArgs) {
+        deferReply(true)
+        reply(httpClient.get("https://api.ipify.org").bodyAsText(), ephemeral = true)
+    }
+}

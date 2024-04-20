550e887c83b85972956a1bcf50fddea7aa730f59
Flo <tectoast@gmail.com>
2024-04-20 19:48:12 +0200

    refactor: delete unused CCData

--- src/main/kotlin/de/tectoast/emolga/utils/json/emolga/customcommand/CCData.kt
+++
@@ -1,6 +0,0 @@
-package de.tectoast.emolga.utils.json.emolga.customcommand
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class CCData(var image: String? = null, var text: String? = null)

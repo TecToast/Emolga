0903932c9875accf73e31292aeedfb1ae6be45ee
Flo <tectoast@gmail.com>
2025-01-31 21:23:18 +0100

    feat: wooloo draft

--- src/main/kotlin/de/tectoast/emolga/utils/json/emolga/draft/Wooloo.kt
+++ src/main/kotlin/de/tectoast/emolga/utils/json/emolga/draft/Wooloo.kt
@@ -1,5 +1,7 @@
 package de.tectoast.emolga.utils.json.emolga.draft
 
+import de.tectoast.emolga.utils.RequestBuilder
+import de.tectoast.emolga.utils.records.CoordXMod
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -7,4 +9,10 @@ import kotlinx.serialization.Serializable
 @SerialName("Wooloo")
 class Wooloo : League() {
     override val teamsize = 12
+    override val pickBuffer = 7
+
+    override suspend fun RequestBuilder.pickDoc(data: PickData) {
+        newSystemPickDoc(data)
+        addSingle(data.roundIndex.CoordXMod("Draft", Int.MAX_VALUE, 2, 3, 0, 4 + data.indexInRound), data.pokemon)
+    }
 }

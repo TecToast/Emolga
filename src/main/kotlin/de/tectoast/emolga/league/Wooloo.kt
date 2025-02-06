02104bd5e5aae62f0ecdb059097169f79eab03c3
Flo <tectoast@gmail.com>
2025-02-06 21:18:33 +0100

    feat: wooloo doc

--- src/main/kotlin/de/tectoast/emolga/league/Wooloo.kt
+++ src/main/kotlin/de/tectoast/emolga/league/Wooloo.kt
@@ -1,15 +1,35 @@
 package de.tectoast.emolga.league
 
+import de.tectoast.emolga.utils.DocEntry
 import de.tectoast.emolga.utils.RequestBuilder
+import de.tectoast.emolga.utils.records.Coord
 import de.tectoast.emolga.utils.records.CoordXMod
+import de.tectoast.emolga.utils.records.SorterData
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 @SerialName("Wooloo")
 class Wooloo : League() {
     override val teamsize = 12
     override val pickBuffer = 7
+
+    @Transient
+    override val docEntry = DocEntry.create(this) {
+        newSystem(SorterData("Tabelle!C6:J13", newMethod = true, cols = listOf(7, 6, 4))) {
+            b.addSingle(
+                if (gdi == 6) Coord("Spielplan", "E", 22 + index) else gdi.CoordXMod(
+                    "Spielplan",
+                    2,
+                    4,
+                    3,
+                    6,
+                    4 + index
+                ), defaultGameplanString
+            )
+        }
+    }
 
     override suspend fun RequestBuilder.pickDoc(data: PickData) {
         newSystemPickDoc(data)

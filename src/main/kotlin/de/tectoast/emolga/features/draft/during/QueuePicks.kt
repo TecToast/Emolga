92ccf5204a61b90e53a8612dec11b634805d59a8
Flo <tectoast@gmail.com>
2025-03-01 12:35:04 +0100

    feat: QueuePicks for Loewe

--- src/main/kotlin/de/tectoast/emolga/features/draft/during/QueuePicks.kt
+++ src/main/kotlin/de/tectoast/emolga/features/draft/during/QueuePicks.kt
@@ -14,7 +14,7 @@ object QueuePicks {
     object Command : CommandFeature<NoArgs>(
         NoArgs(), CommandSpec(
             "queuepicks", "Verwalte deine gequeueten Picks", Constants.G.ASL, Constants.G.NDS,
-            Constants.G.EPP
+            Constants.G.EPP, Constants.G.LOEWE
         )
     ) {
 

8bb7a39b48daaee67c121bd1d7a728b3e3ee0cbe
Flo <tectoast@gmail.com>
2025-03-08 13:27:01 +0100

    feat: WPP DocEntry

--- src/main/kotlin/de/tectoast/emolga/league/WPP.kt
+++ src/main/kotlin/de/tectoast/emolga/league/WPP.kt
@@ -1,11 +1,15 @@
 package de.tectoast.emolga.league
 
 import de.tectoast.emolga.bot.jda
+import de.tectoast.emolga.utils.BasicStatProcessor
+import de.tectoast.emolga.utils.DocEntry
 import de.tectoast.emolga.utils.RequestBuilder
+import de.tectoast.emolga.utils.indexedBy
 import de.tectoast.emolga.utils.records.CoordXMod
 import dev.minn.jda.ktx.coroutines.await
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 @SerialName("WPP")
@@ -14,6 +18,14 @@ class WPP : League() {
 
     override val duringTimerSkipMode = NEXT_PICK
     override val afterTimerSkipMode = AFTER_DRAFT_ORDERED
+
+    @Transient
+    override val docEntry = DocEntry.create(this) {
+        monsOrder = { list -> list.sortedBy { it.tier.indexedBy(tierlist.order) }.map { it.name } }
+        killProcessor = BasicStatProcessor {
+            plindex.CoordXMod("Kader", 2, 'P' - 'B', 5 + gdi, 19, monindex + 9)
+        }
+    }
 
     override suspend fun RequestBuilder.pickDoc(data: PickData) {
         addSingle(data.roundIndex.CoordXMod("Draft", 4, 5, 3, 11, 4 + data.indexInRound), data.pokemon)

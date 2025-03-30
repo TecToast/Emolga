93633b10d3ac46e7cecf7d6c2fdc2aa1d73197c6
Flo <tectoast@gmail.com>
2025-03-30 09:41:13 +0200

    refactor: add JVM breakpoint comment

--- src/main/kotlin/de/tectoast/emolga/features/various/ControlCentralButton.kt
+++ src/main/kotlin/de/tectoast/emolga/features/various/ControlCentralButton.kt
@@ -29,7 +29,7 @@ object ControlCentralButton : ButtonFeature<ControlCentralButton.Args>(::Args, B
         }
         reply("Done!")
         if (breakpoint) {
-            Unit
+            Unit // I have a JVM breakpoint here
         }
     }
 }

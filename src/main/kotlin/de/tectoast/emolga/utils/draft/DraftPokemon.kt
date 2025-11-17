a79c3f89cac143219c6e5d1be707db34438d21d1
Flo <tectoast@gmail.com>
2025-11-18 00:10:53 +0100

    feat: config for DraftPokemon

--- src/main/kotlin/de/tectoast/emolga/utils/draft/DraftPokemon.kt
+++ src/main/kotlin/de/tectoast/emolga/utils/draft/DraftPokemon.kt
@@ -1,11 +1,16 @@
 package de.tectoast.emolga.utils.draft
 
+import de.tectoast.emolga.utils.json.Config
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Config("DraftPokemon", "Ein gepicktes Pokemon")
 data class DraftPokemon(
+    @Config("Name", "Der Name des Pokemons")
     var name: String = "NAME",
+    @Config("Tier", "Das Tier des Pokemons")
     var tier: String = "TIER",
+    @Config("Free", "Ob das Pokemon ein Freepick ist")
     var free: Boolean = false,
     var quit: Boolean = false,
     var noCost: Boolean = false

6b90e70d5b1f3474f46b52b9d602f83b6804f80c
Flo <tectoast@gmail.com>
2025-11-18 17:11:34 +0100

    feat: Kosmo league

---
+++ src/main/kotlin/de/tectoast/emolga/league/Kosmo.kt
@@ -0,0 +1,28 @@
+package de.tectoast.emolga.league
+
+import de.tectoast.emolga.utils.BasicStatProcessor
+import de.tectoast.emolga.utils.DocEntry
+import de.tectoast.emolga.utils.records.Coord
+import de.tectoast.emolga.utils.records.CoordXMod
+import de.tectoast.emolga.utils.y
+
+class Kosmo : League() {
+    override val teamsize = 10
+
+    override val docEntry = DocEntry.create(this) {
+        killProcessor = BasicStatProcessor {
+            Coord("Teamstatistik", gdi.y(3, 12), plindex.y(21, monindex + 4))
+        }
+        deathProcessor = BasicStatProcessor {
+            Coord("Teamstatistik", gdi.y(3, 13), plindex.y(21, monindex + 4))
+        }
+
+        resultCreator = {
+            val baseCoord = gdi.CoordXMod("Spielplan", 3, 'F' - 'B', 3, 11, 6 + index * 2)
+            b.addSingle(
+                baseCoord, defaultGameplanStringWithoutUrl
+            )
+            b.addSingle(baseCoord.plusY(1), url)
+        }
+    }
+}

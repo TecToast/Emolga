f59592711c1b6d5574256bffcb22d75fc33a739a
Flo <tectoast@gmail.com>
2025-11-18 18:39:52 +0100

    fix: make Kosmo serializable

--- src/main/kotlin/de/tectoast/emolga/league/Kosmo.kt
+++ src/main/kotlin/de/tectoast/emolga/league/Kosmo.kt
@@ -5,10 +5,16 @@ import de.tectoast.emolga.utils.DocEntry
 import de.tectoast.emolga.utils.records.Coord
 import de.tectoast.emolga.utils.records.CoordXMod
 import de.tectoast.emolga.utils.y
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
+@Serializable
+@SerialName("Kosmo")
 class Kosmo : League() {
     override val teamsize = 10
 
+    @Transient
     override val docEntry = DocEntry.create(this) {
         killProcessor = BasicStatProcessor {
             Coord("Teamstatistik", gdi.y(3, 12), plindex.y(21, monindex + 4))

d3aee01df71bd016bfe00dd0bbd0ac48382a3c7f
Flo <tectoast@gmail.com>
2026-02-17 10:07:57 +0100

    fix: add delay to ladder tournament sd user fetching

--- src/main/kotlin/de/tectoast/emolga/utils/json/Emolga.kt
+++ src/main/kotlin/de/tectoast/emolga/utils/json/Emolga.kt
@@ -1106,6 +1106,7 @@ data class LadderTournament(
 
     private suspend fun fetchDataForUsers(): Map<Long, SDUserResponse> {
         return users.filter { it.value.verified }.mapValues {
+            delay(2000)
             httpClient.get("https://pokemonshowdown.com/users/${it.value.sdName.toUsername()}.json")
                 .body<SDUserResponse>()
         }

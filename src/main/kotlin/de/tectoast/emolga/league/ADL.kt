ed35054a774ca448dd232cfb4c1bf3cf3262d804
Flo <tectoast@gmail.com>
2026-02-27 15:56:49 +0100

    feat: ADL doc (for now)

--- src/main/kotlin/de/tectoast/emolga/league/ADL.kt
+++ src/main/kotlin/de/tectoast/emolga/league/ADL.kt
@@ -1,10 +1,30 @@
 package de.tectoast.emolga.league
 
+import de.tectoast.emolga.utils.DataTypeForMon
+import de.tectoast.emolga.utils.DocEntry
+import de.tectoast.emolga.utils.StatProcessor
+import de.tectoast.emolga.utils.records.Coord
+import de.tectoast.emolga.utils.records.CoordXMod
+import de.tectoast.emolga.utils.y
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 @SerialName("ADL")
 class ADL : League() {
     override val teamsize = 10
+
+    @Transient
+    override val docEntry = DocEntry.create(this) {
+        +StatProcessor {
+            Coord("Kills (type in)", 9 + gdi, memIdx.y(15, 3 + monIndex())) to DataTypeForMon.KILLS
+        }
+        resultCreator = {
+            b.addRow(
+                gdi.CoordXMod("Schedule", 3, 8, 4, 7, 3 + index),
+                buildDefaultSplitGameplanString("vs.")
+            )
+        }
+    }
 }

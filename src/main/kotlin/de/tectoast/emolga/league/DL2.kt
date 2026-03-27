08b407af37e789407ab95ce89facd9acd92ae49f
Flo <tectoast@gmail.com>
2026-03-27 14:49:21 +0100

    fix: DL2 pick doc

--- src/main/kotlin/de/tectoast/emolga/league/DL2.kt
+++ src/main/kotlin/de/tectoast/emolga/league/DL2.kt
@@ -11,6 +11,6 @@ class DL2 : League() {
     override val teamsize = 11
 
     override suspend fun RequestBuilder.pickDoc(data: PickData) {
-        addSingle(data.idx.CoordXMod("rosterdata", 5, 7, 5, 34, 3), data.pokemon)
+        addSingle(data.idx.CoordXMod("rosterdata", 5, 7, 5, 34, data.picks.size + 2), data.pokemon)
     }
 }

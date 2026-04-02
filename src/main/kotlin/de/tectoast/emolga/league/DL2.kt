b6676c8d6340bc00c0e462f06975cfeabd8584b3
Flo <tectoast@gmail.com>
2026-04-02 23:26:36 +0200

    feat: DL2 doc

--- src/main/kotlin/de/tectoast/emolga/league/DL2.kt
+++ src/main/kotlin/de/tectoast/emolga/league/DL2.kt
@@ -1,15 +1,27 @@
 package de.tectoast.emolga.league
 
-import de.tectoast.emolga.utils.RequestBuilder
+import de.tectoast.emolga.utils.*
+import de.tectoast.emolga.utils.records.Coord
 import de.tectoast.emolga.utils.records.CoordXMod
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 @SerialName("DL2")
 class DL2 : League() {
     override val teamsize = 11
     override val duringTimerSkipMode = NEXT_PICK
+
+    @Transient
+    override val docEntry = DocEntry.create(this) {
+        +StatProcessor {
+            Coord("Killstats", gdi.y(3, 15), memIdx.y(21, 4 + monIndex())) to DataTypeForMon.KILLS
+        }
+        +StatProcessor {
+            Coord("Killstats", gdi.y(3, 16), memIdx.y(21, 4 + monIndex())) to DataTypeForMon.DEATHS
+        }
+    }
 
     override suspend fun RequestBuilder.pickDoc(data: PickData) {
         addSingle(data.idx.CoordXMod("rosterdata", 5, 7, 5, 34, data.picks.size + 2), data.pokemon)

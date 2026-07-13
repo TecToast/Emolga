ec1fc3f091f0ce0fda91da4faf1248adbbc44b67
Flo <tectoast@gmail.com>
2026-07-13 23:10:09 +0200

    refactor: move whole BotConfig to DI

--- src/main/kotlin/de/tectoast/emolga/di/ConfigModule.kt
+++ src/main/kotlin/de/tectoast/emolga/di/ConfigModule.kt
@@ -10,36 +10,36 @@ import java.io.File
 
 @Module
 class ConfigModule {
-    private val botConfig: BotConfig by lazy {
-        Json.decodeFromString(
-            File(System.getenv("CONFIG_FILE") ?: "config.json").readText()
-        )
-    }
 
     @Single
-    fun google(): BotConfig.Google = botConfig.google
+    fun botConfig(): BotConfig = Json.decodeFromString(
+        File(System.getenv("CONFIG_FILE") ?: "config.json").readText()
+    )
+
+    @Single
+    fun google(botConfig: BotConfig): BotConfig.Google = botConfig.google
 
     @Single
     @Named("discordToken")
-    fun discord(): String = botConfig.discord
+    fun discord(botConfig: BotConfig): String = botConfig.discord
 
     @Single
     @Named("discordFlegmonToken")
-    fun discordFlegmon(): OptionalString =
+    fun discordFlegmon(botConfig: BotConfig): OptionalString =
         botConfig.discordflegmon?.let { OptionalString.Present(it) } ?: OptionalString.Absent
 
     @Single
-    fun subscriber(): BotConfig.Subscriber = botConfig.subscriber
+    fun subscriber(botConfig: BotConfig): BotConfig.Subscriber = botConfig.subscriber
 
     @Single
-    fun database(): BotConfig.Database = botConfig.database
+    fun database(botConfig: BotConfig): BotConfig.Database = botConfig.database
 
     @Single
-    fun oauth2(): BotConfig.Oauth2 = botConfig.oauth2
+    fun oauth2(botConfig: BotConfig): BotConfig.Oauth2 = botConfig.oauth2
 
     @Single
-    fun staticCloud(): BotConfig.StaticCloud = botConfig.staticCloud
+    fun staticCloud(botConfig: BotConfig): BotConfig.StaticCloud = botConfig.staticCloud
 
     @Single
-    fun botConstants(): BotConstants = botConfig.botConstants
+    fun botConstants(botConfig: BotConfig): BotConstants = botConfig.botConstants
 }

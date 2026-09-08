89705a16235ecae54cdc685e4fb78c3918ff56f5
Flo <flo@tectoast.de>
2026-09-08 20:02:25 +0200

    feat: show user data for user signup option in WebUI

--- src/main/kotlin/de/tectoast/emolga/domain/league/signup/service/SignUpParticipantService.kt
+++ src/main/kotlin/de/tectoast/emolga/domain/league/signup/service/SignUpParticipantService.kt
@@ -11,12 +11,15 @@ import org.koin.core.annotation.Single
 @Single
 class SignupParticipantService(
     private val signupRepo: SignupRepository,
-    private val discordUserService: DiscordUserService
+    private val discordUserService: DiscordUserService,
 ) {
     suspend fun getParticipantsForSignup(guild: Long, identifier: String): ParticipantDataGet? {
         val signup = signupRepo.getLeagueSignup(guild, identifier) ?: return null
         val allEntries = signupRepo.getAllEntries(signup.id)
-        val relevantUsers = allEntries.values.flatMap { it.users }
+        val relevantUsers = allEntries.values.flatMap {
+            it.users + it.data.entries.filter { (k, _) -> k.startsWith("user_") }
+                .mapNotNull { (_, v) -> v.toLongOrNull() }
+        }
         val userData = discordUserService.getData(guild, relevantUsers)
         val result = allEntries.map { (id, it) ->
             ParticipantData(
@@ -25,7 +28,15 @@ class SignupParticipantService(
                     val data = userData[u]
                     UserData(u.toString(), data?.displayName ?: "N/A", data?.avatarUrl ?: "")
                 },
-                data = it.data,
+                data = it.data.mapValues { (k, v) ->
+                    if (k.startsWith("user_")) {
+                        val userId = v.toLongOrNull() ?: return@mapValues v
+                        val data = userData[userId]
+                        data?.displayName ?: v
+                    } else {
+                        v
+                    }
+                },
                 hasLogo = it.logoIdentifier != null,
                 conference = it.conference,
             )

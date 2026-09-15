12ca156a2e38a3ec14700d8bd37aa2781ca98c4d
Flo <flo@tectoast.de>
2026-09-15 18:04:20 +0200

    fix: restrict ping substitutes to unique users

--- src/main/kotlin/de/tectoast/emolga/domain/league/member/service/LeagueMentionService.kt
+++ src/main/kotlin/de/tectoast/emolga/domain/league/member/service/LeagueMentionService.kt
@@ -9,11 +9,11 @@ class LeagueMentionService(private val leagueMemberRepo: LeagueMemberRepository)
     suspend fun getMentionForParticipant(leagueName: String, idx: Int): MessageMentionData {
         val participants = leagueMemberRepo.getParticipantsForIdx(leagueName, idx)
         val (allSubstitute, main) = participants.partition { it.substitute }
-        val pingSubstitutes = allSubstitute.filter { it.shouldPing }
+        val pingSubstitutes = allSubstitute.filterTo(mutableSetOf()) { it.shouldPing }
         val mainContent = main.joinToString { "<@${it.userId}>" }
         val substituteContent =
             if (pingSubstitutes.isNotEmpty()) " ||(${pingSubstitutes.joinToString { "<@${it.userId}>" }})||" else ""
-        val enabledMentions = participants.filter { it.shouldPing }.map { it.userId }
+        val enabledMentions = participants.filterTo(mutableSetOf()) { it.shouldPing }.map { it.userId }
         return MessageMentionData(mainContent + substituteContent, enabledMentions)
     }
 }

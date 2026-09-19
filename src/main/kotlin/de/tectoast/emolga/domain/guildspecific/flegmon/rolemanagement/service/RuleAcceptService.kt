1ba3d25de197eb033d7d1bd8812801a3b764fce5
Flo <flo@tectoast.de>
2026-09-19 15:24:58 +0200

    fix: flegmon roles

--- src/main/kotlin/de/tectoast/emolga/domain/guildspecific/flegmon/rolemanagement/service/RuleAcceptService.kt
+++ src/main/kotlin/de/tectoast/emolga/domain/guildspecific/flegmon/rolemanagement/service/RuleAcceptService.kt
@@ -1,16 +1,20 @@
 package de.tectoast.emolga.domain.guildspecific.flegmon.rolemanagement.service
 
-import de.tectoast.emolga.discord.GuildMemberRepository
+import de.tectoast.emolga.discord.JDAGuildMemberRepository
+import de.tectoast.emolga.discord.OptionalJDA
+import de.tectoast.emolga.discord.jdaOrNull
 import de.tectoast.emolga.domain.guildspecific.flegmon.rolemanagement.model.AcceptRulesResult
+import org.koin.core.annotation.Named
 import org.koin.core.annotation.Single
 
 private const val ACCEPTED_RULES_ROLE = 605635673885507614
 
 @Single
-class RuleAcceptService(private val memberRepo: GuildMemberRepository) {
+class RuleAcceptService(@Named("flegmon") private val flegmonJda: OptionalJDA) {
+    val memberRepo = flegmonJda.jdaOrNull?.let { JDAGuildMemberRepository(it) }
     suspend fun acceptRules(guildId: Long, userId: Long, currentRoleIds: List<Long>): AcceptRulesResult {
         if (ACCEPTED_RULES_ROLE in currentRoleIds) return AcceptRulesResult.AlreadyAccepted
-        memberRepo.addRole(guildId, userId, ACCEPTED_RULES_ROLE)
+        memberRepo?.addRole(guildId, userId, ACCEPTED_RULES_ROLE)
         return AcceptRulesResult.Accepted
     }
 }
--- src/main/kotlin/de/tectoast/emolga/domain/guildspecific/flegmon/rolemanagement/service/SelfRoleService.kt
+++ src/main/kotlin/de/tectoast/emolga/domain/guildspecific/flegmon/rolemanagement/service/SelfRoleService.kt
@@ -1,15 +1,22 @@
 package de.tectoast.emolga.domain.guildspecific.flegmon.rolemanagement.service
 
-import de.tectoast.emolga.discord.GuildMemberRepository
+import de.tectoast.emolga.discord.JDAGuildMemberRepository
+import de.tectoast.emolga.discord.OptionalJDA
+import de.tectoast.emolga.discord.jdaOrNull
 import de.tectoast.emolga.domain.guildspecific.flegmon.rolemanagement.repository.FlegmonRoleRepository
+import org.koin.core.annotation.Named
 import org.koin.core.annotation.Single
 
 
 @Single
-class SelfRoleService(private val memberRepo: GuildMemberRepository, private val roleRepo: FlegmonRoleRepository) {
+class SelfRoleService(
+    @Named("flegmon") private val flegmonJda: OptionalJDA,
+    private val roleRepo: FlegmonRoleRepository
+) {
+    val memberRepo = flegmonJda.jdaOrNull?.let { JDAGuildMemberRepository(it) }
     suspend fun setNewSelfRoles(guildId: Long, userId: Long, newRoleCompIds: List<String>) {
         val (add, remove) = roleRepo.getRoles().partition { it.compId in newRoleCompIds }.toList()
             .map { it.map { r -> r.roleId } }
-        memberRepo.modifyRoles(guildId, userId, add, remove)
+        memberRepo?.modifyRoles(guildId, userId, add, remove)
     }
 }
